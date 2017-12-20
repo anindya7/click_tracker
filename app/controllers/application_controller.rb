afdd3e17ecd4fe16a27cb46642d44a6ee94070bd
anindya7 <anindyachanda7@gmail.com>
2017-12-20 19:24:00 +0530

    Removed event triger on visit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,15 +5,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def track_action
-<<<<<<< HEAD
     # if ahoy.visit
-    #   ahoy.track_visit
+    #   ahoy.track "Viewed #{controller_path}##{action_name}", params: request.path_parameters
     # end
-=======
-    if ahoy.visit
-      ahoy.track "Viewed #{controller_path}##{action_name}", params: request.path_parameters
-    end
->>>>>>> c74bd9a1a56ae1d319ebe35b25ac7b1a061a52ec
   end
   
 end
