3ce851713fa123dfb2caddc0d32e4c1a4f14f387
anindya7 <anindyachanda7@gmail.com>
2017-12-26 17:20:21 +0530

    Updated ahoy store

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,3 +1,9 @@
 class Ahoy::Store < Ahoy::Stores::ActiveRecordStore  
-  
+  Ahoy.quiet = false
+  Ahoy.track_visits_immediately = true
+  Ahoy.geocode = :async
+
+  def exclude?
+    user ? false : bot?
+  end
 end 
